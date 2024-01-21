d8eccba04f36ffa67494c11bc162206268767bbe
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-21 20:37:52 +0530

    building UI game page

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 class GamePage extends StatelessWidget {
-  const GamePage({super.key});
+  double? _deviceHeight, _deviceWidth;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceHeight! * 0.05,
+          ),
+          child: _gameUI(),
+        ),
+      ),
+    );
+  }
+
+  Widget _gameUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _questionText(),
+      ],
+    );
+  }
+
+  Widget _questionText() {
+    return const Text(
+      "Text Question 1, Nothing Interesting",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+        fontWeight: FontWeight.w400,
+      ),
+    );
   }
 }
