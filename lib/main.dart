bac4dba07c44eb07fc45d3c7c9b8d300d6488b91
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-25 22:55:58 +0530

    Adding provider

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Frivia',
       theme: ThemeData(
-        fontFamily: 'ArchitectsDaughters',
+        fontFamily: 'ArchitectsDaughter',
         scaffoldBackgroundColor: const Color.fromRGBO(
           31,
           31,
